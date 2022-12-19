f1ecabc4995b101c40c0b20f1a2a19f20ebbbfb9
Matheus Deperon  - Analista de Sistemas - Júnior - QA - Junior <78362152+analistadeperon@users.noreply.github.com>
2022-12-19 10:21:44 -0300

    Create estado.cs

---
+++ estado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodeCoverage.Prompt.Dto
+{
+    public class StateDto
+    {
+        public StateDto(Guid id,
+                        string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
