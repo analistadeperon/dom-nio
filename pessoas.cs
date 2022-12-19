85848cec4ba30758174501dd4edac9a89ce8eca7
Matheus Deperon  - Analista de Sistemas - Júnior - QA - Junior <78362152+analistadeperon@users.noreply.github.com>
2022-12-19 10:21:15 -0300

    Create pessoas.cs

---
+++ pessoas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CodeCoverage.Domain.Entities
+{
+    public class Person
+    {
+        public Person(Guid id,
+                      string name,
+                      Guid? cityId,
+                      Guid? stateId,
+                      City city,
+                      State state)
+        {
+            Id = id;
+            Name = name;
+            CityId = cityId;
+            StateId = stateId;
+            City = city;
+            State = state;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+        public Guid? CityId { get; }
+        public Guid? StateId { get; set; }
+
+        #region Relationships
+
+        public virtual City City { get; }
+        public virtual State State { get; }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
