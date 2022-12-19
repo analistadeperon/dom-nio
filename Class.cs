41c1e3438ecfb962e7dd3efb823de47ac8794977
Matheus Deperon  - Analista de Sistemas - Júnior - QA - Junior <78362152+analistadeperon@users.noreply.github.com>
2022-12-19 10:20:17 -0300

    Create Class.cs

---
+++ Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+namespace CodeCoverage.Domain.Entities
+{
+    public class State
+    {
+        public State(Guid id,
+                     string name)
+        {
+            Id = id;
+            Name = name;
+        }
+ 
+        public Guid Id { get; }
+        public string Name { get; }
+ 
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
