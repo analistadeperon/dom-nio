2de96feacbbd11d30b8bdb45e0b1a4c4d3b5a0b3
Matheus Deperon  - Analista de Sistemas - Júnior - QA - Junior <78362152+analistadeperon@users.noreply.github.com>
2022-12-19 10:20:43 -0300

    Create city.cs

---
+++ city.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CodeCoverage.Domain.Entities
+{
+    public class City
+    {
+        public City(Guid id,
+                    string name,
+                    Guid stateId,
+                    State state)
+        {
+            Id = id;
+            Name = name;
+            StateId = stateId;
+            State = state;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+        public Guid StateId { get; }
+
+        #region Relationships
+
+        public virtual State State { get; }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
