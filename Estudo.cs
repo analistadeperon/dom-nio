7b30903a4a7ec42e757223a8890fb46b05b38974
Matheus Deperon  - Analista de Sistemas - Júnior - QA - Junior <78362152+analistadeperon@users.noreply.github.com>
2022-12-19 10:24:56 -0300

    Create Estudo.cs

---
+++ Estudo.cs
@@ -0,0 +1,16 @@
+PS C:\> Set-Location .\GitHub\
+PS C:\GitHub> New-Item -ItemType Directory -Path CakeAutomation
+
+    Diretório: C:\GitHub
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+d-----       27/01/2018     12:26                CakeAutomation
+
+PS C:\GitHub> Set-Location .\CakeAutomation\
+PS C:\GitHub\CakeAutomation>
+
+
+
+
+um arquivo chamado build.ps1 que roda no Powershell.
