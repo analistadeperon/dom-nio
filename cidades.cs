437cd011978fdb7b6066c284b4f27baae038c9d7
Matheus Deperon  - Analista de Sistemas - Júnior - QA - Junior <78362152+analistadeperon@users.noreply.github.com>
2022-12-19 10:22:18 -0300

    Create cidades.cs

---
+++ cidades.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CodeCoverage.Prompt.Dto
+{
+    public class CityDto
+    {
+        public CityDto(Guid id,
+                       string name,
+                       Guid stateId,
+                       StateDto stateDto)
+        {
+            Id = id;
+            Name = name;
+            StateId = stateId;
+            State = stateDto;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+        public Guid StateId { get; }
+
+        #region Relationships
+
+        public virtual StateDto State { get; }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
